c8fe646522190679a11fc73be4ab6f25bdba98ed
Andrii Derkach <andrsj.derkach@gmail.com>
2023-03-12 21:18:05 +0200

    JWT refactored

--- internal/delivery/http/handlers/jwt.go
+++ internal/delivery/http/handlers/jwt.go
@@ -1,40 +1,136 @@
 package handlers
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 	"time"
 
 	jwt "github.com/golang-jwt/jwt/v5"
+
+	"github.com/andrsj/feedback-service/pkg/logger"
+
 )
 
-// TODO Refactor this shit
-func (h *handlers) Token(w http.ResponseWriter, r *http.Request) {
-	h.logger.Info("Hit status endpoint", nil)
+const (
+	defaultMinutes    = 10
+	defaultRole       = "all"
+	minutesQueryParam = "minutes"
+	roleQueryParam    = "role"
+	tokenPrefix       = "Bearer"
+)
 
-	token, err := generateJWTToken()
+var (
+	errRoleParam    = errors.New("invalid role parameter")
+	errMinutesParam = errors.New("invalid minutes parameter")
+)
+
+func (h *handlers) Token(w http.ResponseWriter, r *http.Request) {
+	var (
+		minutes int64
+		role    string
+		err     error
+		errMSG  string
+	)
+
+	queryParams := r.URL.Query()
+
+	h.logger.Info("Hit Token endpoint", logger.M{"queryParams": queryParams})
+
+	minutes, err = checkMinutes(queryParams)
 	if err != nil {
-		http.Error(w, "Can't create a token :(", http.StatusInternalServerError)
+		errMSG = "Error while checking minutes"
+		h.logger.Error(errMSG, logger.M{"err": err})
+		http.Error(w, errMSG, http.StatusBadRequest)
 
 		return
 	}
 
+	role, err = checkRole(queryParams)
+	if err != nil {
+		errMSG = "Error while checking role"
+		h.logger.Error(errMSG, logger.M{"err": err})
+		http.Error(w, errMSG, http.StatusBadRequest)
+
+		return
+	}
+
+	h.logger.Info("Received data", logger.M{
+		"role": role,
+		"time": minutes,
+	})
+
+	token, err := generateJWTToken(minutes, role)
+	if err != nil {
+		errMSG = "Can't create a token :("
+		h.logger.Error(errMSG, logger.M{"err": err})
+		http.Error(w, errMSG, http.StatusInternalServerError)
+
+		return
+	}
+
+	fullToken := fmt.Sprintf("%s %s", tokenPrefix, token)
+
+	h.logger.Info("Successfully returning created token", logger.M{"token": fullToken})
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Baerer %s", token)
+	fmt.Fprint(w, fullToken)
 }
 
-func generateJWTToken() (string, error) {
+func checkRole(queryParams url.Values) (string, error) {
+	role := queryParams.Get(roleQueryParam)
+	if role != "" {
+		switch role {
+		case "get", "post", "all":
+			return role, nil
+		default:
+			return "", fmt.Errorf("wrong role '%s': %w", role, errRoleParam)
+		}
+	}
+
+	return defaultRole, nil
+}
+
+func checkMinutes(queryParams url.Values) (int64, error) {
+	var (
+		err     error
+		minutes int64 = defaultMinutes
+	)
+
+	minutesStr := queryParams.Get(minutesQueryParam)
+	if minutesStr != "" {
+		minutes, err = strconv.ParseInt(minutesStr, 10, 64)
+		if err != nil {
+			return 0, fmt.Errorf("wrong minutes param: %w", errMinutesParam)
+		}
+
+		if minutes <= 0 {
+			return 0, fmt.Errorf("wrong value for minutes param '%d': %w", minutes, errMinutesParam)
+		}
+	}
+
+	return minutes, nil
+}
+
+func generateJWTToken(minutes int64, role string) (string, error) {
+	const (
+		expiredAtKey = "expiredAt"
+		roleKey      = "role"
+	)
+
 	secret := []byte(os.Getenv("SECRET"))
 
-	expirationTime := time.Now().Add(1 * time.Minute)
+	expirationTime := time.Now().Add(time.Duration(minutes) * time.Minute)
 
 	claims := jwt.MapClaims{
-		"expiredAt": expirationTime.Unix(),
-		"role":      "all",
+		expiredAtKey: expirationTime.Unix(),
+		roleKey:      role,
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
 	tokenString, err := token.SignedString(secret)
 	if err != nil {
 		return "", fmt.Errorf("failed to sign token: %w", err)
--- internal/delivery/http/middlewares/middlewares.go
+++ internal/delivery/http/middlewares/middlewares.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -11,6 +12,23 @@ import (
 
 	"github.com/andrsj/feedback-service/internal/infrastructure/cache"
 	"github.com/andrsj/feedback-service/pkg/logger"
+
+)
+
+const (
+	authorizationHeader = "Authorization"
+	tokenPrefix         = "Bearer"
+)
+
+var (
+	errMissingAuthHeader   = errors.New("missing authorization header")
+	errInvalidAuthHeader   = errors.New("invalid authorization header, use 'Bearer <Token>'")
+	errSigningMethod       = errors.New("unexpected signing method")
+	errTokenExpired        = errors.New("token is expired")
+	errTokenMissingExpired = errors.New("token missing expired value")
+	errTokenRole           = errors.New("token has wrong role")
+	errTokenMissingRole    = errors.New("token missing role value")
+	errInvalidToken        = errors.New("invalid token")
 )
 
 func CacheMiddleware(cache cache.Cache) func(next http.Handler) http.Handler {
@@ -41,112 +59,136 @@ func CacheMiddleware(cache cache.Cache) func(next http.Handler) http.Handler {
 	}
 }
 
-// TODO Refactor this shit
 func JWTMiddleware(log logger.Logger) func(next http.Handler) http.Handler {
+	var errMSG string
+
 	log = log.Named("jwt")
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			authHeader := r.Header.Get("Authorization")
-
-			log.Info("Authorization", logger.M{"token": authHeader})
-
-			if authHeader == "" {
-				log.Error("Missing authorization header", nil)
-				http.Error(w, "Missing authorization header", http.StatusUnauthorized)
-
-				return
-			}
-
-			// if authHeader != os.Getenv("SECRET") {
-			// 	log.Error("Wrong authorization header", nil)
-			// 	http.Error(w, "Wrong authorization header", http.StatusUnauthorized)
-
-			// 	return
-			// }
-
-			bearerToken := strings.Split(authHeader, " ")
-			if len(bearerToken) != 2 {
-				log.Error("Invalid authorization header", logger.M{"token": authHeader})
-				http.Error(w, "Invalid authorization header, use 'Bearer <Token>'", http.StatusUnauthorized)
-
-				return
-			}
-
-			tokenStr := bearerToken[1]
-
-			token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-				}
-
-				return []byte(os.Getenv("SECRET")), nil
-			})
-
+			tokenStr, err := validateHeaders(r.Header)
 			if err != nil {
-				log.Error("Wrong authorization header", nil)
-				http.Error(w, "Wrong authorization header", http.StatusUnauthorized)
-			}
+				errMSG = "Wrong authorization header"
+				log.Error(errMSG, logger.M{"err": err})
+				http.Error(w, errMSG, http.StatusBadRequest)
 
-			if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-				if expiredAt, ok := claims["expiredAt"].(float64); ok {
-
-					log.Info("expiredAt", logger.M{"expiredAt": expiredAt})
-					log.Info("expiredAt", logger.M{"expiredAt": int64(expiredAt)})
-
-					if int64(expiredAt) < time.Now().Unix() {
-						log.Error("Token is expired", logger.M{"expiredAt": expiredAt})
-						http.Error(w, "token is expired", http.StatusUnauthorized)
-
-						return
-					}
-				} else {
-					log.Error("ExpiredAt is missing", nil)
-					http.Error(w, "ExpiredAt is missing", http.StatusUnauthorized)
-
-					return
-				}
-
-				if role, ok := claims["role"].(string); ok {
-
-					log.Info("role", logger.M{"role": role})
-
-					switch role {
-					case "reading":
-						if r.Method != "GET" {
-							log.Error("Token has invalid role", logger.M{"role": role})
-							http.Error(w, "token has invalid role", http.StatusUnauthorized)
-
-							return
-						}
-					case "creating":
-						if r.Method != "POST" {
-							log.Error("Token has invalid role", logger.M{"role": role})
-							http.Error(w, "token has invalid role", http.StatusUnauthorized)
-
-							return
-						}
-					case "all":
-						// allow everything
-					default:
-						log.Error("Token has invalid role", logger.M{"role": role})
-						http.Error(w, "token has invalid role", http.StatusUnauthorized)
-
-						return
-					}
-
-				} else {
-					log.Error("Role is missing", nil)
-					http.Error(w, "role is missing", http.StatusUnauthorized)
-
-					return
-				}
-
-			} else {
-				log.Error("Invalid token claims", nil)
-				http.Error(w, "invalid token claims", http.StatusUnauthorized)
 				return
 			}
+
+			token, err := parseToken(tokenStr)
+			if err != nil {
+				errMSG = "Wrong token authorization"
+				log.Error(errMSG, nil)
+				http.Error(w, errMSG, http.StatusUnauthorized)
+
+				return
+			}
+
+			err = validateToken(token, r)
+			if err != nil {
+				errMSG = "Validating token error"
+				log.Error(errMSG, logger.M{"err": err})
+				http.Error(w, errMSG, http.StatusUnauthorized)
+
+				return
+			}
+
+			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+func parseToken(tokenStr string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("wrong alg '%v': %w", token.Header["alg"], errSigningMethod)
+		}
+
+		return []byte(os.Getenv("SECRET")), nil
+	})
+
+	if err != nil {
+		return token, fmt.Errorf("parsing error: %w", err)
+	}
+
+	return token, nil
+}
+
+func validateHeaders(header http.Header) (string, error) {
+	const lenBearerToken = 2
+
+	authHeader := header.Get(authorizationHeader)
+	if authHeader == "" {
+		return "", errMissingAuthHeader
+	}
+
+	// token format: "Bearer_<Token>"
+	splittedBearerToken := strings.Split(authHeader, " ")
+	if len(splittedBearerToken) != lenBearerToken {
+		return "", errInvalidAuthHeader
+	}
+
+	if splittedBearerToken[0] != tokenPrefix {
+		return "", errInvalidAuthHeader
+	}
+
+	return splittedBearerToken[1], nil
+}
+
+func validateToken(token *jwt.Token, r *http.Request) error {
+	var err error
+
+	claims, claimsIsValid := token.Claims.(jwt.MapClaims)
+	if !claimsIsValid && !token.Valid {
+		return errInvalidToken
+	}
+
+	err = validateExpiredAt(claims)
+	if err != nil {
+		return fmt.Errorf("validating 'expiredAt' error: %w", err)
+	}
+
+	err = validateRole(claims, r.Method)
+	if err != nil {
+		return fmt.Errorf("validating 'role' error: %w", err)
+	}
+
+	return nil
+}
+
+func validateRole(claims jwt.MapClaims, httpMethod string) error {
+	role, roleIsValid := claims["role"].(string)
+	if !roleIsValid {
+		return fmt.Errorf("%w", errTokenMissingRole)
+	}
+
+	switch role {
+	case "get":
+		if httpMethod != http.MethodGet {
+			return fmt.Errorf("wrong role for 'GET': %w", errTokenRole)
+		}
+	case "post":
+		if httpMethod != http.MethodPost {
+			return fmt.Errorf("wrong role for 'POST': %w", errTokenRole)
+		}
+	case "all":
+		return nil
+	default:
+		return fmt.Errorf("not existing role role: %w", errTokenRole)
+	}
+
+	return nil
+}
+
+func validateExpiredAt(claims jwt.MapClaims) error {
+	expiredAt, expiredAtIsValid := claims["expiredAt"].(float64)
+	if !expiredAtIsValid {
+		return fmt.Errorf("%w", errTokenMissingExpired)
+	}
+
+	if int64(expiredAt) < time.Now().Unix() {
+		return fmt.Errorf("expired (%d): %w", time.Now().Unix()-int64(expiredAt), errTokenExpired)
+	}
+
+	return nil
 }
